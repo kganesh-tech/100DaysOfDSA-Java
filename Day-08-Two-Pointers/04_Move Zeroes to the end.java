dafdd8cfc097cbac5c72ec99764db5a36fcea530
kganesh-tech <k.ganesh6286@gmail.com>
2026-03-30 09:59:44 +0530

    Update 04_Move Zeroes to the end.java

--- Day-08-Two-Pointers/04_Move Zeroes to the end.java
+++ Day-08-Two-Pointers/04_Move Zeroes to the end.java
@@ -1,1 +1,31 @@
+problem : Move Zeroes to the end
+Pattern: Two Pointers
+Time Complexity: 0(n)
+Space Complexity: 0(1)
 
+  import java.util.*;
+public class moveZeroes {
+
+    public static void moveZeroes(int[] arr) {
+
+        int slow = 0;
+
+        for (int fast = 0; fast < arr.length; fast++) {
+
+            if(arr[fast]!= 0) {
+                int temp = arr[slow];
+                arr[slow] = arr[fast];
+                arr[fast] = temp;
+                slow++;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = { 1, 0, 2, 0, 3, 0};
+         moveZeroes(arr);
+         
+         for(int num: arr) {
+             System.out.println(num + " ");
+         }
+    }
+}
