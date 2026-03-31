df86eb6b60d9db04e339779fcc1f92b65e405ca5
kganesh-tech <k.ganesh6286@gmail.com>
2026-03-31 09:46:55 +0530

    Update remove elements with the value.java

--- Day-09-Two-Pointers/remove elements with the value.java
+++ Day-09-Two-Pointers/remove elements with the value.java
@@ -1,1 +1,38 @@
+Problem: remove elements with the value
+pattern: Two Pointer
+Time Complexity: 0(n)
+Space Complexity: 0(1)
 
+  import java.util.*;
+public class RemoveElement {
+
+    public static int removeElement(int[] arr, int val) {
+
+        int slow = 0;
+
+        for (int fast = 0; fast < arr.length; fast++) {
+
+            if (arr[fast] != val) {
+                arr[slow] = arr[fast];
+                slow++;
+            }
+        }
+
+        return slow;   // new length of array after removing val
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {3, 2, 2, 3};
+        int val = 3;
+
+        int newLength = removeElement(arr, val);
+
+        System.out.println("New Length: " + newLength);
+
+        System.out.print("Array after removing element: ");
+        for (int i = 0; i < newLength; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
