cfd1aeff2949c109e6b1aa54bc55125b1c26d239
kganesh-tech <k.ganesh6286@gmail.com>
2026-04-01 09:39:39 +0530

    Update reverse vowels in the string.java

--- Day-10-Two-Pointers/reverse vowels in the string.java
+++ Day-10-Two-Pointers/reverse vowels in the string.java
@@ -1,1 +1,46 @@
+//Problem: reverse a vowels in the string
+//Pattern: Two Pointers
+//Time Complexity: 0(n)
+//Space Complexity: 0(1)
 
+import java.util.*;
+
+public class ReverseVowels {
+    public static boolean isVowel (char c) {
+        c = Character.toLowerCase(c);
+         return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+    
+    public static String reverseVowels(String s) {
+        
+        char[] arr = s.toCharArray();
+        
+        int left =0;
+        int right =arr.length - 1;
+        
+        while (left < right) {
+            
+            while(left < right && ! isVowel(arr[left])) {
+                 left++; 
+            }
+            while(left < right && ! isVowel(arr[right])) {
+                right--;
+            }
+            
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            
+            left++;
+            right--;
+        }
+        return new String(arr);
+    }
+    
+    public static void main(String[] arr) {
+        String s = "hello";
+        
+        String result = reverseVowels(s);
+        System.out.println(result);
+    }
+}
