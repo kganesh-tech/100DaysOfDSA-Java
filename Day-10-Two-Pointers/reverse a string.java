43cf09a02f9021b826d5d20643cf5d98a4db3c9d
kganesh-tech <k.ganesh6286@gmail.com>
2026-04-01 09:35:58 +0530

    Update reverse a string.java

--- Day-10-Two-Pointers/reverse a string.java
+++ Day-10-Two-Pointers/reverse a string.java
@@ -1,1 +1,30 @@
+//problem: reverse a string
+//Pattern: Two Pointers
+//Time Complexity: 0(n)
+//Space Complexity: 0(1)
 
+import java.util.*;
+
+public class ReverseString {
+    public static void reverseString (char[] s) {
+        int left =0;
+        int right = s.length - 1;
+        
+        while (left < right) {
+            
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+        
+        
+        left++;
+        right--;
+    }
+}
+
+public static void main(String[] args) {
+    char[] s = { 'h', 'e', 'l', 'l', '0'};
+    reverseString(s);
+    System.out.println(Arrays.toString(s));
+}
+}
