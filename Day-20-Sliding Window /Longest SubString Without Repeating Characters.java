9da5ce288cfb6376dcaca938152043284886796b
kganesh-tech <k.ganesh6286@gmail.com>
2026-04-12 20:10:33 +0530

    Update Longest SubString Without Repeating Characters.java

--- Day-20-Sliding Window /Longest SubString Without Repeating Characters.java
+++ Day-20-Sliding Window /Longest SubString Without Repeating Characters.java
@@ -1,13 +1,50 @@
-/*
-Problem: Longest Substring Without Repeating Characters
 
-Approach:
+// Problem: Longest Substring Without Repeating Characters
+
+// Approach:
 - Use sliding window with two pointers (left, right)
 - Use HashSet to store unique characters
 - Expand window using right pointer
 - If duplicate found, shrink window from left until valid
 - Track maximum length of valid window
 
-Time Complexity: O(n)
-Space Complexity: O(1)
-*/
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+
+  import java.util.*;
+
+class Solution {
+    public int lengthofLongestSubString (String s) {
+        int left = 0;
+        int maxLength = 0;
+        Set<Character> set = new HashSet<>();
+        
+        for(int right = 0; right < s.length(); right++) {
+            
+            char ch = s.charAt(right);
+            
+            while(set.contains(ch)) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            
+            set.add(ch);
+            
+            maxLength = Math.max(maxLength , right - left + 1);
+            
+        }
+        
+        return maxLength;
+    }
+    
+    public static void main (String[] args) {
+        Solution obj =new Solution();
+        String s = "pwwkew";
+        
+        int result = obj.lengthofLongestSubString(s);
+        
+        System.out.println(result);
+        
+    }
+}
