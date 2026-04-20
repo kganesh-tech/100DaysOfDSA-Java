a15a651400c2d5c5ba208714aae6a8b953cfc797
kganesh-tech <k.ganesh6286@gmail.com>
2026-04-20 20:12:17 +0530

    Update 34_FindRepeatedDNA Sequences.java

--- Day-27-Sliding Window/34_FindRepeatedDNA Sequences.java
+++ Day-27-Sliding Window/34_FindRepeatedDNA Sequences.java
@@ -1,1 +1,20 @@
+class Solution {
+  public List<String> findRepeatedDnaSequences (String s) {
+    int k = 10;
 
+    Set<String> seen = new HashSet<>();
+    Set<String> result = new HashSet<>();
+
+    for (int i = 0; i< s.length() - k; i++) {
+      String sub = s.substring (i, i + k);
+
+      if(seen.contains(sub)) {
+        result.add(sub);
+      } else {
+        seen.add(sub);
+      }
+    }
+    return new ArrayList<>(result);
+  }
+}
+    
