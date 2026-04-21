35c16bb9c9c3f76f9ef0e5024480fd916b79a240
kganesh-tech <k.ganesh6286@gmail.com>
2026-04-21 19:31:15 +0530

    Update 35_ Defuse the Bomb.java

--- Day-28-Sliding Winodw( Circular Array + Brute force)/35_ Defuse the Bomb.java
+++ Day-28-Sliding Winodw( Circular Array + Brute force)/35_ Defuse the Bomb.java
@@ -1,1 +1,32 @@
+class Solution {
+  public {
+    public int[] decrypt(int[] code, int k) {
+
+        int n = code.length;
+        int[] res = new int[n];
+
+        if(k == 0) return res;
+
+     
+        for (int i = 0; i < n; i++) {
+          int sum = 0;
+
+           if(k > 0) {
+             for (int j = 1; i < k; j++) {
+               int idx = (i + j) % n;
+
+               sum += code[idx];
+             } else {
+             for(int j =1; j < -k; j++) {
+               int idx = (i - j + n) % n;
+               sum += code[idx];
+             }
+           }
+          res[i] = sum;
+        } 
+
+      return res;
+    }
+  }
+             
 
