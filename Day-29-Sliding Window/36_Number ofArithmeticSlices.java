d644671f48d63f369ae6d95b28a112a0bfdb72b3
kganesh-tech <k.ganesh6286@gmail.com>
2026-04-22 22:24:44 +0530

    Update 36_Number ofArithmeticSlices.java

--- Day-29-Sliding Window/36_Number ofArithmeticSlices.java
+++ Day-29-Sliding Window/36_Number ofArithmeticSlices.java
@@ -1,1 +1,16 @@
+class Solution {
+  public static int NumberOfArithmeticSlices (int[] nums) {
+    int count = 0;
+    int total = 0;
 
+  for(int i = 0; i< nums.length; i++) {
+    if(nums[i - 1) - nums[i] == nums[i - 2] - nums[i - 1]) {
+         count ++;
+         total += count;
+    } else {
+      count = 0;
+    }
+  }
+    return total;
+  }
+}
