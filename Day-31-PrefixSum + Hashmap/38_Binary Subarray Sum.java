a9c3f44d473f32c99995d85d7d264bf850f2e91f
kganesh-tech <k.ganesh6286@gmail.com>
2026-04-28 22:44:30 +0530

    Update 38_Binary Subarray Sum.java

--- Day-31-PrefixSum + Hashmap/38_Binary Subarray Sum.java
+++ Day-31-PrefixSum + Hashmap/38_Binary Subarray Sum.java
@@ -1,1 +1,36 @@
+import java.util.HashMap;
 
+public class BinarySubarraysWithSum {
+
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1); 
+
+        int sum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            sum += num;
+
+            
+            if (map.containsKey(sum - goal)) {
+                count += map.get(sum - goal);
+            }
+
+            
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+
+    
+    public static void main(String[] args) {
+        BinarySubarraysWithSum obj = new BinarySubarraysWithSum();
+        int[] nums = {1, 0, 1, 0, 1};
+        int goal = 2;
+
+        System.out.println(obj.numSubarraysWithSum(nums, goal)); 
+    }
+}
