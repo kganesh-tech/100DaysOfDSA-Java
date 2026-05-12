9a1bb1af800086b04b409c85fca749d9c302234d
kganesh-tech <k.ganesh6286@gmail.com>
2026-05-12 10:53:58 +0530

    Update 40_ Longest Repeating Character Replacement.java

--- Day-33-Sliding Window/40_ Longest Repeating Character Replacement.java
+++ Day-33-Sliding Window/40_ Longest Repeating Character Replacement.java
@@ -1,1 +1,29 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] count = new int[26];
+        int left = 0;
+        int maxLen = 0;
+        int maxFreq = 0;
+
+       for( int right = 0; right < s.length(); right++) {
+        count[s.charAt(right) - 'A'] ++;
+        maxFreq = Math.max(maxFreq, count[s.charAt(right) - 'A']);
+
+        int windowLength = right - left + 1;
+
+        while ( windowLength - maxFreq > k) {
+            count[s.charAt(left) - 'A']--;
+
+            left++;
+
+            windowLength = right - left + 1;
+        }
+
+        maxLen = Math.max(maxLen, windowLength);
+
+       }
+
+       return maxLen;
+        }
+    }
 
